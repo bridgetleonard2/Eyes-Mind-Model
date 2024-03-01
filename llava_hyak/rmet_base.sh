0a95237ed9bfb79ec1e29dd94d13c45a36a45e54
Bridget Leonard <leonardbridget01@gmail.com>
2024-03-01 14:57:50 -0800

    adding data

--- llava_hyak/rmet_base.sh
+++ llava_hyak/rmet_base.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+# Start by clearing cache
+rm -R .cache/huggingface
+
+# Set up cache directories:
+export APPTAINER_CACHEDIR="/gscratch/scrubbed/${USER}/.cache/apptainer"
+export HUGGINGFACE_HUB_CACHE="/gscratch/scrubbed/${USER}/.cache/huggingface"
+mkdir -p "${APPTAINER_CACHEDIR}" "${HUGGINGFACE_HUB_CACHE}"
+
+# Set up Apptainer:
+export APPTAINER_BIND=/gscratch APPTAINER_WRITABLE_TMPFS=1 APPTAINER_NV=1
 
 # Define questions for the task
 questions=()
@@ -20,21 +30,23 @@ done
 
 # Assuming both arrays have the same length
 for ((i = 0; i < ${#questions[@]}; i++)); do
-    echo "Question ${i}: ${questions[i]}, Image file: ${image_files[i]}"
     prompt="Choose which word best describes what the person in the picture is thinking or feeling based on just their eyes alone. You may feel that more than one word is applicable, but please choose just one word, the word which you consider to be most suitable. Your 4 choices are: ${questions[i]}"
     echo "$prompt"
 
     image_path="$directory/${image_files[i]}"
     echo "$image_path"
 
-    apptainer run \
-    --bind llava_hyak/output:/container/output \
+    output=$(apptainer run \
     --bind llava_hyak/rmet_materials:/container/rmet_materials \
-    oras://ghcr.io/uw-psych/llava-container/llava-container:latest \
+    oras://ghcr.io/uw-psych/llava-container/llava-container-train:latest \
     llava-run \
     --model-path liuhaotian/llava-v1.5-13b \
     --query "$prompt" \
-    --image-file "$image_path" \
-    | tail -n 1 \
-    | tee -a llava_hyak/rmet_results/rmet_base-2.txt # "tee" writes the output to output.json while also printing it on the screen
+    --image-file "$image_path" 2>&1)
+    
+    # Print the entire output to the screen
+    echo "$output"
+
+    # Append only the last line of the output to the file
+    echo "$output" | tail -n 1 | tee -a llava_hyak/rmet_results/rmet_base-2.txt # "tee" writes the output to output.json while also printing it on the screen
 done
